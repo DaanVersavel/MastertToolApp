11bd256e80c122bb715343ec56903c890805a4b7
DaanVersavel <92854110+DaanVersavel@users.noreply.github.com>
2022-04-30 13:59:17 +0200

    beginning

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>TADA</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Login from "./components/Login";
+import React from "react";
+
+
+
+export default class App extends React.Component {
+    render()
+    {
+        return (
+          <View style={styles.container}>
+              <Login />
+
+          </View>
+        );
+    }
+
+
+    // <View style={styles.container}>
+    //   <Text>TADA</Text>
+    //   <StatusBar style="auto" />
+    // </View>
 }
 
 const styles = StyleSheet.create({
